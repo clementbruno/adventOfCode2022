5ea0c4afe5d852e7ceb135aa2b79f7cfadead128
clementbruno <clmnt.bruno@gmail.com>
2022-12-04 13:59:46 +0100

    Day 4

---
+++ day4/solution.ts
@@ -0,0 +1,44 @@
+import { readFileSync } from "fs";
+
+const file: string = readFileSync("day4/input.txt", "utf8");
+
+const pairs: number[][][] = file
+  .split("\n")
+  .slice(0, -1) // Dirty but needed to get rid of automatically inserted newline at the end of input text file
+  .map((line) =>
+    line.split(",").map((pair) => pair.split("-").map((el) => Number(el)))
+  );
+
+// --------------------
+// Part 1
+// --------------------
+
+const fullyContainedPairs: number[][][] = pairs.filter(
+  ([firstElement, secondElement]) => {
+    return (
+      (firstElement[0] <= secondElement[0] &&
+        firstElement[1] >= secondElement[1]) ||
+      (secondElement[0] <= firstElement[0] &&
+        secondElement[1] >= firstElement[1])
+    );
+  }
+);
+
+console.log(fullyContainedPairs.length); // 526
+
+// --------------------
+// Part 2
+// --------------------
+
+const overlappingPairs: number[][][] = pairs.filter(
+  ([firstElement, secondElement]) => {
+    return (
+      (firstElement[0] <= secondElement[0] &&
+        firstElement[1] >= secondElement[0]) ||
+      (secondElement[0] <= firstElement[0] &&
+        secondElement[1] >= firstElement[0])
+    );
+  }
+);
+
+console.log(overlappingPairs.length); // 886
